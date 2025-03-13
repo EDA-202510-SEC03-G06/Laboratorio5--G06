0f20865d4c3219f836bc343556674179c4c97946
Laura Alvarado <la.alvaradoo1@uniandes.edu.co>
2025-03-12 21:28:28 -0500

    View

--- App/view.py
+++ App/view.py
@@ -54,6 +54,7 @@ def print_menu():
     """
     print("Bienvenido")
     #TODO: agregar opción 0 para escoger el tipo de estructura de datos y opción 5 para seleccionar el algoritmo de ordenamiento
+    print("0- Escoger el tipo de estructura de datos") 
     print("1- Cargar información en el catálogo")
     print("2- Consultar la información de un libro")
     print("3- Consultar los libros de un autor")
@@ -145,6 +146,7 @@ def print_sort_results(sort_books, sample=3):
             # Obtener el libro en la posición actual.
             book = data_structure.get_element(sorted_books, book_pos)
             # TODO: Completar la lógica para imprimir la información del libro.
+            print_book_info(book)
             # Disminuir el contador de la muestra.
             sample -= 1
 
@@ -191,7 +193,11 @@ def main():
             print("Cargando información de los archivos ....")
             bk, at, tg, bktg = load_data(control)
             #TODO: imprimir la cantidad de libros, autores, géneros y asociaciones de géneros a libros cargados
-
+            print(f" Libros cargados: {bk}")
+            print(f" Autores cargados: {at}")
+            print(f" Géneros cargados: {tg}")
+            print(f" Asociación libro-género: {bktg}")
+    
         elif int(inputs[0]) == 2:
             number = input("Ingrese el id del libro que desea buscar: ")
             book = logic.get_book_info_by_book_id(control, int(number))
@@ -223,6 +229,8 @@ def main():
             result = logic.sort_books(control)
             #TODO:imprimir el resultado del ordenamiento 
             print("Tiempo de ejecución:", f"{result[1]:.3f}", "[ms]")
+            print(" Libros ordenados:")
+            print_sort_results(result) 
 
         elif int(inputs[0]) == 8:
             # confirmar salida del programa
